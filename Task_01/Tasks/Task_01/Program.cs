c10555c63ee32435471cfccce3c58c256e3338b4
Mohammed-Maher7 <mohammed.maher.1110@gmail.com>
2025-07-22 00:42:12 +0300

    Task_01

---
+++ Task_01/Tasks/Task_01/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Hello, Please enter the maximum number you need to add");
+
+string? maximumNumberFromUser;
+
+maximumNumberFromUser = Console.ReadLine();
+
+// Validate input
+if (string.IsNullOrWhiteSpace(maximumNumberFromUser) || !int.TryParse(maximumNumberFromUser, out int maximumNumber))
+{
+    Console.WriteLine("Invalid input. Please enter a valid integer.");
+    return;
+}
+
+Console.WriteLine($"Are you sure you need to get the addition up to {maximumNumber}?");
+
+
+int sum = 0;
+
+//Time complexity: O(n) 
+for (int i = 1; i <= maximumNumber; i++)
+{
+    sum += i;
+}
+
+
+// Time complexity: O(c)  Where c is constant
+int seriesSum = maximumNumber * (maximumNumber + 1) / 2;
+
+Console.WriteLine($"Sum using loop = {sum}");
+Console.WriteLine("Time complexity using loop = O(n)");
+Console.WriteLine($"Sum using formula = {seriesSum}");
+Console.WriteLine("Time complexity using formula = O(c) Where c is constant");
---
+++ Task_01/Tasks/Task_02/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+
+Console.WriteLine("Hello, Please enter the maximum number you need to write");
+
+string? maximumNumberFromUser;
+maximumNumberFromUser  = Console.ReadLine();
+int maximumNumber;
+
+if (!int.TryParse(maximumNumberFromUser, out maximumNumber) || maximumNumber < 1)
+{
+    Console.WriteLine("Invalid input. Please enter a positive number.");
+    return;
+}
+
+string numbers = string.Empty;
+
+Stopwatch stopwatch = Stopwatch.StartNew();
+
+
+//Time complexity: O(n^2)
+for (int i = 1; i <= maximumNumber; i++)
+{
+    numbers = $"{numbers}{i},";
+}
+
+stopwatch.Stop();
+
+ 
+// Remove Comma
+if (numbers.EndsWith(","))
+{
+    numbers = numbers.TrimEnd(',');
+}
+
+Console.WriteLine(numbers);
+Console.WriteLine($"Elapsed Seconds = {stopwatch.Elapsed.TotalSeconds}");
+
+
+/*
+ * StringBuilder uses a dynamic char[] buffer that grows (usually doubles) only when needed. 
+ * Time complexity: O(n)
+ **/
